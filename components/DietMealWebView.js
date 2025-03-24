9a7e2eadd98123e18934b9ffaf6b2352eca781f4
kkulkkul-diet <kaystar.play@gmail.com>
2025-03-24 11:14:07 +0700

    Add files via upload

---
+++ components/DietMealWebView.js
@@ -0,0 +1,4 @@
+// 실제 다이어트 식단 컴포넌트는 여기에 삽입됩니다.
+export default function DietMealWebView() {
+  return <div>식단표 로딩 중...</div>
+}
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+
+const DietMealWebView = dynamic(() => import('../components/DietMealWebView'), { ssr: false });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>꿀꿀 다이어트 식단</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <DietMealWebView />
+    </>
+  );
+}
