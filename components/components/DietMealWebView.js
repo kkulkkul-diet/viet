e7a9efba11dbfdc8cf255b1100431b684eaafb20
kkulkkul-diet <kaystar.play@gmail.com>
2025-03-24 11:57:12 +0700

    Add files via upload

---
+++ components/components/DietMealWebView.js
@@ -0,0 +1,8 @@
+export default function DietMealWebView() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>월간 다이어트 식단표</h1>
+      <p>식단표 로딩 중... (구성된 탭 형태 식단표를 여기에 삽입하세요)</p>
+    </div>
+  )
+}
